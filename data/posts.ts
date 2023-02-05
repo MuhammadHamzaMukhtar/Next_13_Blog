c38bb3e34c2b7e37a6c8a229ddfd96b0cfe197e9
Muhammad Hamza Mukhtar <101422276+MuhammadHamzaMukhtar@users.noreply.github.com>
2023-02-05 16:08:12 +0500

    live class project

---
+++ data/posts.ts
@@ -0,0 +1,4 @@
+export const posts = [
+    {'id': '1', 'title': 'Two forms of Pre Rendering', "date": '2020-01-22'},
+    {'id': '2', 'title': 'When to use Static Generation vs Server Side Rendering', "date": '2020-01-23'},
+]
